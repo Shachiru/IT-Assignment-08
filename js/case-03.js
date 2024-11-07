91f4ed92e8a9fb31bdf3eb62b93910967e225bcf
Shachiru Rashmika <shachirurashmika35@gmail.com>
2024-11-07 17:31:00 +0530

    index finshed

--- js/case-03.js
+++ js/case-03.js
@@ -1,42 +1,41 @@
-const audio = document.getElementById("audio-file");
-const start = document.getElementById("btn-start");
-const end = document.getElementById("btn-end");
-const lights = document.querySelectorAll(".light");
-
-let currentLight = 0;
+const music = document.getElementById("audio-file");
+const light_count = 7;
+let current_light = 0;
 let direction = "RIGHT";
-let intervalID = null;
-const lightCount = lights.length;
+let intervalId = null;
 
-start.addEventListener("click", () => {
-  audio.play();
-  intervalID = setInterval(updateLight, 100);
-});
-
-end.addEventListener("click", () => {
-  audio.pause();
-  clearInterval(intervalID);
-});
-
-function updateLight() {
-  lights.forEach((light) => (light.style.background = "white"));
-  lights[currentLight].style.background = "red";
-  if (direction === "RIGHT" && currentLight > 0) {
-    lights[currentLight - 1].style.background = "pink";
-  } else if (direction === "LEFT" && currentLight < lightCount - 1) {
-    lights[currentLight + 1].style.background = "pink";
-  }
-
+updateLight = () => {
   if (direction === "RIGHT") {
-    currentLight++;
-    if (currentLight === lightCount - 1) {
+    current_light++;
+    $(".light").css("background", "white");
+    $(".light").eq(current_light).css("background", "red");
+    $(".light")
+      .eq(current_light - 1)
+      .css("background", "pink");
+
+    if (current_light == light_count - 1) {
       direction = "LEFT";
     }
   } else {
-    currentLight--;
-    if (currentLight === 0) {
+    current_light--;
+    $(".light").css("background", "white");
+    $(".light").eq(current_light).css("background", "red");
+    $(".light")
+      .eq(current_light + 1)
+      .css("background", "pink");
+
+    if (current_light === 0) {
       direction = "RIGHT";
     }
   }
-}
+};
 
+$("#btn-start").on("click", function () {
+  intervalId = setInterval(updateLight, 100);
+  music.play();
+});
+
+$("#btn-end").on("click", function () {
+  clearInterval(intervalId);
+  music.pause();
+});
